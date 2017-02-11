8a8aacab4510fe2e4603abd32a3d98232283fe5c
bigwheel <k.bigwheel+eng@gmail.com>
2017-02-11 09:27:28 +0900

    Add main method

---
+++ src/main/scala/com/github/bigwheel/thpy/Main.scala
@@ -0,0 +1,11 @@
+package com.github.bigwheel.thpy
+
+import com.github.bigwheel.thpy.Macro.anyToTyped
+
+// for human interactive test purpose Main.
+object Main {
+  def main(args: Array[String]): Unit = {
+    val a = 10
+    break(a)
+  }
+}
