a024050f9b40ccf2c87b5377c41f32f420b9d840
bigwheel <k.bigwheel+eng@gmail.com>
2017-02-18 15:47:05 +0900

    Change macro code style to macro bundle

--- macro/src/main/scala/com/github/bigwheel/thpy/Macro.scala
+++ macro/src/main/scala/com/github/bigwheel/thpy/Macro.scala
@@ -5,16 +5,19 @@ import scala.tools.nsc.interpreter.NamedParam
 
 object Macro {
 
-  implicit def anyToTyped[T](value: T): NamedParam.Typed[T] = macro anyToTypedImpl[T]
+  private class MacroImpl(val c: Context) {
 
-  // macro implementation must be public at least in scala 2.12
-  def anyToTypedImpl[T: c.WeakTypeTag](c: Context)(value: c.Tree): c.Tree = {
-    import c.universe._
-    val T = weakTypeOf[T]
-    val paramRep = show(value)
-    val paramRepTree = Literal(Constant(paramRep))
-    val paramRepExpr = c.Expr[String](paramRepTree)
-    q"new _root_.scala.tools.nsc.interpreter.NamedParam.Typed[$T]($paramRepExpr, $value)"
+    def apply[T: c.WeakTypeTag](value: c.Tree): c.Tree = {
+      import c.universe._
+      val T = weakTypeOf[T]
+      val paramRep = show(value)
+      val paramRepTree = Literal(Constant(paramRep))
+      val paramRepExpr = c.Expr[String](paramRepTree)
+      q"new _root_.scala.tools.nsc.interpreter.NamedParam.Typed[$T]($paramRepExpr, $value)"
+    }
+
   }
+
+  implicit def anyToTyped[T](value: T): NamedParam.Typed[T] = macro MacroImpl.apply[T]
 
 }
