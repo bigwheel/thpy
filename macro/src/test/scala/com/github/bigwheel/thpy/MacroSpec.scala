c5c0ff26adf5775b1fbe3a0d3d0b9d640abfa1c5
bigwheel <k.bigwheel+eng@gmail.com>
2017-02-18 15:52:02 +0900

    Remove supersafe plugin for code portability

--- macro/src/test/scala/com/github/bigwheel/thpy/MacroSpec.scala
+++ macro/src/test/scala/com/github/bigwheel/thpy/MacroSpec.scala
@@ -10,7 +10,7 @@ class MacroSpec extends FunSpec with Matchers {
   // Following code has type erasue problem then this Equality is not completed.
   private[this] implicit val _ = new Equality[NamedParam.Typed[Int]] {
     override def areEqual(lhs: NamedParam.Typed[Int], rhs: Any): Boolean = rhs match {
-      case rhs2 : NamedParam.Typed[Int] @unchecked =>
+      case rhs2 : NamedParam.Typed[Int] =>
         lhs.tpe == rhs2.tpe && lhs.name == rhs2.name && lhs.value == rhs2.value
       case _ =>
         false
