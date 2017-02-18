d10d2818b5b3af8c2459a6bfbc4deebc2541e171
bigwheel <k.bigwheel+eng@gmail.com>
2017-02-18 16:16:21 +0900

    Tidy macro test

--- macro/src/test/scala/com/github/bigwheel/thpy/MacroSpec.scala
+++ macro/src/test/scala/com/github/bigwheel/thpy/MacroSpec.scala
@@ -1,26 +1,21 @@
 package com.github.bigwheel.thpy
 
-import org.scalactic.Equality
 import org.scalatest.FunSpec
 import org.scalatest.Matchers
 import scala.tools.nsc.interpreter.NamedParam
 
 class MacroSpec extends FunSpec with Matchers {
 
-  // Following code has type erasue problem then this Equality is not completed.
-  private[this] implicit val _ = new Equality[NamedParam.Typed[Int]] {
-    override def areEqual(lhs: NamedParam.Typed[Int], rhs: Any): Boolean = rhs match {
-      case rhs2 : NamedParam.Typed[Int] =>
-        lhs.tpe == rhs2.tpe && lhs.name == rhs2.name && lhs.value == rhs2.value
-      case _ =>
-        false
-    }
-  }
-
   it ("can convert NamedParam.Typed implicitly") {
     import Macro.anyToTyped
-    val subject: NamedParam.Typed[Int] = 1
-    subject should equal(new NamedParam.Typed[Int]("1", 1))
+    val a = 10
+    val subject: NamedParam.Typed[Int] = a
+
+    val expect = new NamedParam.Typed[Int]("a", 10)
+
+    subject.name should be(expect.name)
+    subject.value should be(expect.value)
+    subject.tpe should be(expect.tpe)
   }
 
 }
