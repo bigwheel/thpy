ca72ec2f2d8d72f7f9823b8a4a898f4e0de9d328
bigwheel <k.bigwheel+eng@gmail.com>
2017-02-18 15:24:34 +0900

    Tidy around macro code

--- macro/src/main/scala/com/github/bigwheel/thpy/Macro.scala
+++ macro/src/main/scala/com/github/bigwheel/thpy/Macro.scala
@@ -4,8 +4,10 @@ import scala.reflect.macros.whitebox.Context
 import scala.tools.nsc.interpreter.NamedParam
 
 object Macro {
+
   implicit def anyToTyped[T](value: T): NamedParam.Typed[T] = macro anyToTypedImpl[T]
 
+  // macro implementation must be public at least in scala 2.12
   def anyToTypedImpl[T: c.WeakTypeTag](c: Context)(value: c.Tree): c.Tree = {
     import c.universe._
     val T = weakTypeOf[T]
@@ -14,4 +16,5 @@ object Macro {
     val paramRepExpr = c.Expr[String](paramRepTree)
     q"new _root_.scala.tools.nsc.interpreter.NamedParam.Typed[$T]($paramRepExpr, $value)"
   }
+
 }
