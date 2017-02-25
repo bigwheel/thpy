75c379ab5c94e3768e8ea662b532c5767801b8ca
bigwheel <k.bigwheel+eng@gmail.com>
2017-02-25 17:52:25 +0900

    Remove needless codes

--- src/main/scala/com/github/bigwheel/thpy/package.scala
+++ src/main/scala/com/github/bigwheel/thpy/package.scala
@@ -1,15 +1,12 @@
 package com.github.bigwheel
 
 import java.io.BufferedReader
-import scala.tools.nsc._
 import scala.tools.nsc.interpreter._
 
 package object thpy {
 
   private def breakImpl(iilCreater: (NamedParam*) => ImprovedILoop)(namedParams: NamedParam*) = {
     val repl = iilCreater(namedParams: _*)
-    //val sbtClasspath = System.getProperty("sbt-classpath")
-    //s.classpath.value = s".:$sbtClasspath"
 
     repl.process
   }
